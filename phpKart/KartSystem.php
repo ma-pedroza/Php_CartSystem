21098f1803339343ecbc84e2f59e3dbf419b1d1e
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-08-31 19:42:15 -0300

    Commit 1 - Rodrigo
    Titio!

--- phpKart/KartSystem.php
+++ phpKart/KartSystem.php
@@ -13,8 +13,8 @@
             ['id' => 3, 'nome' => 'Memória Ram 8GB', 'preco' => 259.99, 'estoque' => 15],
             ['id' => 4, 'nome' => 'Memória Ram 16GB', 'preco' => 500.99, 'estoque' => 50],
             ['id' => 5, 'nome' => 'Memória Ram 32GB', 'preco' => 899.99, 'estoque' => 31],
-            ['id' => 6, 'nome' => 'Placa de Vídeo NVDIA RTX 3060 12GB', 'preco' => 1.799.99, 'estoque' => 113],
-            ['id' => 7, 'nome' => 'Placa de Vídeo AMD RX 6600 8GB', 'preco' => 1.499.99, 'estoque' => 92]
+            ['id' => 6, 'nome' => 'Placa de Vídeo NVDIA RTX 3060 12GB', 'preco' => 1799.99, 'estoque' => 113],
+            ['id' => 7, 'nome' => 'Placa de Vídeo AMD RX 6600 8GB', 'preco' => 1499.99, 'estoque' => 92]
         ]; 
     }
 
@@ -29,6 +29,19 @@
     }
     public function addKart($id, $quantity){
 
+    }
+
+    public function removeItemFromKart ($id)
+    {
+        $product = getProduct($id)
+        unset($this->kart[id])
+        echo "Item com ID {$id} removido do carrinho.";
+    }
+
+    public function aplicarDesconto($subtotal)
+    {
+        $totalFinal = $subtotal - ($subtotal*0.10);
+        return $totalFinal;
     }
 
 
