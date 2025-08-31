18c2a60fa8617907edac057ac1cbfec4aa0cf898
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-08-31 18:23:19 -0300

    commit Matheus
    Começo do projeto

---
+++ phpKart/KartSystem.php
@@ -0,0 +1,25 @@
+<?php
+
+    class KartSystem{
+
+    private array $products;
+
+    public function __construct(){
+        $this->$products = [
+            ['id' => 1, 'nome' => 'Processador Intel I5', 'preco' => 879.99, 'estoque' => 45],
+            ['id' => 2, 'nome' => 'Processador AMD Ryzen 5', 'preco' => 780.99, 'estoque' => 33],
+            ['id' => 3, 'nome' => 'Memória Ram 8GB', 'preco' => 259.99, 'estoque' => 15],
+            ['id' => 4, 'nome' => 'Memória Ram 16GB', 'preco' => 500.99, 'estoque' => 50],
+            ['id' => 5, 'nome' => 'Memória Ram 32GB', 'preco' => 899.99, 'estoque' => 31],
+            ['id' => 6, 'nome' => 'Placa de Vídeo NVDIA RTX 3060 12GB', 'preco' => 1.799.99, 'estoque' => 113],
+            ['id' => 7, 'nome' => 'Placa de Vídeo AMD RX 6600 8GB', 'preco' => 1.499.99, 'estoque' => 92]
+        ]
+    }
+
+
+}
+
+
+
+
+?>
